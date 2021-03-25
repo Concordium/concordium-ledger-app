1e577fa28705e6ea6aca10207d3eee6fb6b0d655
Jakob Ørhøj <jo@concordium.com>
2021-03-25 09:16:01 +0100

    Document and refactor update found. account

--- src/signUpdateFoundationAccount.h
+++ src/signUpdateFoundationAccount.h
@@ -1,1 +1,11 @@
-void handleSignUpdateFoundationAccount(uint8_t *dataBuffer, volatile unsigned int *flags);
+#ifndef _CONCORDIUM_APP_UPDATE_FOUNDATION_ACCOUNT_H_
+#define _CONCORDIUM_APP_UPDATE_FOUNDATION_ACCOUNT_H_
+
+/**
+ * Handles the signing flow, including updating the display, for the 'update foundation account'
+ * update instruction.
+ * @param cdata please see /doc/ins_update_foundation_account.md for details
+ */
+void handleSignUpdateFoundationAccount(uint8_t *cdata, volatile unsigned int *flags);
+
+#endif
