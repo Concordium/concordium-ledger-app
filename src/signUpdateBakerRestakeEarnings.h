d827509c1ccc53d6631320072c3e22d2326afa12
Jakob Ørhøj <jo@concordium.com>
2021-03-25 10:51:06 +0100

    Fix typo

--- src/signUpdateBakerRestakeEarnings.h
+++ src/signUpdateBakerRestakeEarnings.h
@@ -4,7 +4,7 @@
 /**
  * Handles the signing flow, including updating the display, for the 'update baker restake earnings'
  * account transaction.
- * @param cdata please see /doc/ins_update_baker_restake_earnings
+ * @param cdata please see /doc/ins_update_baker_restake_earnings.md
  */
 void handleSignUpdateBakerRestakeEarnings(uint8_t *cdata, volatile unsigned int *flags);
 
