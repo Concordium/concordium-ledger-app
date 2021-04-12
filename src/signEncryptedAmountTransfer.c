f21eaab0f539930b069cd324672a3b05e08da985
Jakob Ørhøj <jo@concordium.com>
2021-04-12 10:33:28 +0200

    Correct encrypted amount hash sizes

--- src/signEncryptedAmountTransfer.c
+++ src/signEncryptedAmountTransfer.c
@@ -29,8 +29,10 @@ UX_FLOW(ux_sign_encrypted_amount_transfer,
     &ux_sign_encrypted_amount_transfer_2_step
 );
 
-#define P1_INITIAL  0x00
-#define P1_PROOF    0x01
+#define P1_INITIAL                              0x00
+#define P1_REMAINING_AMOUNT                     0x01
+#define P1_TRANSFER_AMOUNT_AGG_INDEX_PROOF_SIZE 0x02
+#define P1_PROOF                                0x03
 
 void handleSignEncryptedAmountTransfer(uint8_t *cdata, uint8_t p1, uint8_t dataLength, volatile unsigned int *flags) {
     if (p1 == P1_INITIAL && tx_state->initialized == false) {
@@ -51,16 +53,26 @@ void handleSignEncryptedAmountTransfer(uint8_t *cdata, uint8_t p1, uint8_t dataL
         }
         ctx->to[50] = '\0';
 
-        // Hash amounts, encrypted amount agg index and size of proof in bytes.
-        cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 26, NULL, 0);
+        ctx->state = TX_ENCRYPTED_AMOUNT_TRANSFER_REMAINING_AMOUNT;
+        ux_flow_init(0, ux_sign_encrypted_amount_transfer, NULL);
+        *flags |= IO_ASYNCH_REPLY;
+    } else if (p1 == P1_REMAINING_AMOUNT && ctx->state == TX_ENCRYPTED_AMOUNT_TRANSFER_REMAINING_AMOUNT) {
+        // Hash remaining amount. Remaining amount is encrypted, and so we cannot display it.
+        cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 192, NULL, 0);
+        ctx->state = TX_ENCRYPTED_AMOUNT_TRANSFER_TRANSFER_AMOUNT;
+        sendSuccessNoIdle();
+    } else if (p1 == P1_TRANSFER_AMOUNT_AGG_INDEX_PROOF_SIZE && ctx->state == TX_ENCRYPTED_AMOUNT_TRANSFER_TRANSFER_AMOUNT) {
+        // Hash transfer amound and agg index. Transfer amount is encrypted, and so we cannot display it.
+        cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 200, NULL, 0);
+        cdata += 200;
 
         // Save proof size so that we know when we are done processing the 
         // proof bytes that we are going to receive.
-        ctx->proofSize = U2BE(cdata, 24);
+        ctx->proofSize = U2BE(cdata, 0);
+        cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 2, NULL, 0);
 
         ctx->state = TX_ENCRYPTED_AMOUNT_TRANSFER_PROOFS;
-        ux_flow_init(0, ux_sign_encrypted_amount_transfer, NULL);
-        *flags |= IO_ASYNCH_REPLY;
+        sendSuccessNoIdle();
     } else if (p1 == P1_PROOF && ctx->state == TX_ENCRYPTED_AMOUNT_TRANSFER_PROOFS) { 
         cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, dataLength, NULL, 0);
         ctx->proofSize -= dataLength;
