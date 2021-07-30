1ea41c5d3388598eccb315068039f9bfd9777c9b
Hjort <shj@concordium.com>
2021-07-30 10:00:51 +0200

    small fixes based on feedback

--- src/signAddIdentityProvider.h
+++ src/signAddIdentityProvider.h
@@ -9,19 +9,19 @@
 void handleSignAddIdentityProvider(uint8_t *cdata, uint8_t p1, uint8_t dataLength, volatile unsigned int *flags, bool isInitialCall);
 
 typedef enum {
-              TX_ADD_IDENTITY_PROVIDER_INITIAL = 35,
-              TX_ADD_IDENTITY_PROVIDER_DESCRIPTION_LENGTH = 36,
-              TX_ADD_IDENTITY_PROVIDER_DESCRIPTION = 37,
-              TX_ADD_IDENTITY_PROVIDER_VERIFY_KEY_LENGTH = 38,
-              TX_ADD_IDENTITY_PROVIDER_VERIFY_KEY = 39,
-              TX_ADD_IDENTITY_PROVIDER_CDI_VERIFY_KEY = 39
+              TX_ADD_IDENTITY_PROVIDER_INITIAL = 40,
+              TX_ADD_IDENTITY_PROVIDER_DESCRIPTION_LENGTH = 41,
+              TX_ADD_IDENTITY_PROVIDER_DESCRIPTION = 42,
+              TX_ADD_IDENTITY_PROVIDER_VERIFY_KEY_LENGTH = 43,
+              TX_ADD_IDENTITY_PROVIDER_VERIFY_KEY = 44,
+              TX_ADD_IDENTITY_PROVIDER_CDI_VERIFY_KEY = 45
 } addIdentityProviderState_t;
 
 typedef struct {
   uint32_t descriptionLength;
   uint32_t verifyKeyLength;
-  uint8_t ipIdentity[4];
-  char cdiVerifyKey[64];
+  uint8_t ipIdentity[5];
+  char cdiVerifyKey[65];
   addIdentityProviderState_t state;
 } signAddIdentityProviderContext_t;
 
