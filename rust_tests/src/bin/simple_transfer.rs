30906d6fe8b44b0fb96e7888ea0391dd263dccbe
Hjort <shj@concordium.com>
2021-09-06 09:42:04 +0200

    Update tests for memo transactions and split into separate files

--- rust_tests/src/bin/simple_transfer.rs
+++ rust_tests/src/bin/simple_transfer.rs
@@ -1,17 +1,12 @@
 mod account_transaction_header;
 mod path;
 
-use std::env;
-
 use hex;
 use ledger::{ApduCommand, LedgerApp};
 use base58check::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let with_memo = args.len() > 1;
-
-    let transaction_kind = if with_memo { "16" } else { "03" };
+    let transaction_kind = "03";
 
     let mut amount = hex::decode("FFFFFFFFFFFFFFFF").unwrap();
     let sender_address = "3C8N65hBwc2cNtJkGmVyGeWYxhZ6R3X77mLWTwAKsnAnyworTq";
@@ -26,34 +21,16 @@ fn main() {
     command_data.append(&mut account_transaction_header::generate_account_transaction_header());
     command_data.append(&mut transaction_payload);
 
-    let p1 = if with_memo { 1 } else { 0 };
-
     let command = ApduCommand {
         cla: 224, // Has to be this value for all commands.
         ins: 2,   // Transfer (Simple one time transfer)
-        p1: p1,
+        p1: 0,
         p2: 0,
         length: 60,
         data: command_data
     };
 
     let ledger = LedgerApp::new().unwrap();
-    let mut result = ledger.exchange(command).expect("Simpler transfer packet failed.");
-
-    if with_memo {
-        let memo  = hex::decode(&args[1]).unwrap();
-        for memo_part in memo.chunks(255) {
-            let memo_command = ApduCommand {
-                cla: 224, // Has to be this value for all commands.
-                ins: 2,   // Transfer (Simple one time transfer)
-                p1: 2,
-                p2: 0,
-                length: memo_part.len() as u8,
-                data: memo_part.to_vec() // test assumes the memo fits
-            };
-            result = ledger.exchange(memo_command).expect("Simpler transfer packet failed.");
-        }
-    }
-
+    let result = ledger.exchange(command).expect("Simpler transfer packet failed.");
     println!("Signature: {}", hex::encode(result.data));
 }
--- rust_tests/src/bin/transfer_with_schedule_and_memo.rs
+++ rust_tests/src/bin/transfer_with_schedule_and_memo.rs
@@ -6,11 +6,12 @@ use hex;
 use ledger::{ApduCommand, LedgerApp};
 
 fn main() {
-    // Transaction kind is 19 -> 13 in hexadecimal.
-    let mut transaction_kind = hex::decode("13").unwrap();
+    let transaction_kind = "18";
 
     let sender_address = "3C8N65hBwc2cNtJkGmVyGeWYxhZ6R3X77mLWTwAKsnAnyworTq";
     let mut receiver_address = sender_address.from_base58check().unwrap().1;
+
+    let memo  = hex::decode("6474657374").unwrap();
 
     // For scheduled amounts we have to split the transaction into multiple packages, as we can
     // only move 255 bytes at a time.
@@ -19,27 +20,42 @@ fn main() {
     // Send initial command.
     // let initial_command_data = format!("{}{}{}{}{}", &path_prefix, &number_of_scheduled_amounts, &transaction_header, &transaction_kind, &to_address);
 
+    // Build transaction payload bytes which is in the initial command.
+    let mut transaction_payload = hex::decode(transaction_kind).unwrap();
+    transaction_payload.append(&mut receiver_address);
+    transaction_payload.append(&mut number_of_scheduled_amounts);
+    transaction_payload.append(&mut (memo.len() as u16).to_be_bytes().to_vec());
+
     let mut initial_command_data = path::generate_key_derivation_path();
     initial_command_data.append(&mut account_transaction_header::generate_account_transaction_header());
-    initial_command_data.append(&mut transaction_kind);
-    initial_command_data.append(&mut receiver_address);
-    initial_command_data.append(&mut number_of_scheduled_amounts);
+    initial_command_data.append(&mut transaction_payload);
 
     let initial_command = ApduCommand {
         cla: 224, // Has to be this value for all commands.
         ins: 3,   // Scheduled transfer
-        p1: 0,
+        p1: 2,
         p2: 0,
         length: 0,
         data: initial_command_data
     };
     let ledger = LedgerApp::new().unwrap();
-    ledger.exchange(initial_command).expect("Initial packet failed.");
+    let mut result = ledger.exchange(initial_command).expect("Initial packet failed.");
 
-    // Generate 17 scheduled transfers to demonstrate that splitting by 15 transfers works.
+    for memo_part in memo.chunks(255) {
+        let memo_command = ApduCommand {
+            cla: 224, // Has to be this value for all commands.
+            ins: 3,
+            p1: 3,
+            p2: 0,
+            length: memo_part.len() as u8,
+            data: memo_part.to_vec()
+        };
+        ledger.exchange(memo_command).unwrap();
+    }
+
+    // Generate 16 scheduled transfers to demonstrate that splitting by 15 transfers works.
     let mut command_data: Vec<u8> = Vec::new();
 
-    println!("The final result received contains the signature.");
     for i in 1..16 {
         let mut scheduled_timestamp = hex::decode("0000017A396883D9").unwrap();
         let mut amount = hex::decode("0000000005F5E100").unwrap();
@@ -60,9 +76,10 @@ fn main() {
                 data: command_data
             };
 
-            let result = ledger.exchange(command).expect("Scheduled transfer command failed");
-            println!("{}", hex::encode(result.data));
+            result = ledger.exchange(command).expect("Scheduled transfer command failed");
             command_data = Vec::new();
         }
     }
+
+    println!("{}", hex::encode(result.data));
 }
