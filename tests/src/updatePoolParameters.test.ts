5f4fbca1a86fdf9c6bdf7b7c4ab68ed2bf51750c
Hjort <shj@concordium.com>
2022-03-22 09:44:16 +0100

    Fix Pool parameters failing sometimes

--- tests/src/updatePoolParameters.test.ts
+++ tests/src/updatePoolParameters.test.ts
@@ -6,13 +6,13 @@ async function poolParameters(sim: Zemu, transport: Transport, images: string, e
     let data = Buffer.from('080000045100000000000000000000000000000000000000020000000000000000000000000000000a00000000000000640000000063de5da700000029' + '0F' + '0000EA30' + '00000200' + '0000EA04', 'hex');
     transport.send(0xe0, 0x41, 0x00, 0x00, data);
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-    await sim.navigateAndCompareSnapshots('.', images + "_initial", [3, 0]);
-    let snapshot = sim.snapshot();
+    await sim.navigateAndCompareSnapshots('.', images + "_initial", [3]);
+    let snapshot = await sim.clickBoth();
     data = Buffer.from('00001001' + '0000EA64' + '0000EA63' + '0000EA65' + '00001000' + '0000EA62', 'hex');
     transport.send(0xe0, 0x41, 0x01, 0x00, data);
     await sim.waitUntilScreenIsNot(snapshot);
-    await sim.navigateAndCompareSnapshots('.', images + "_bounds", [5, 0]);
-    snapshot = sim.snapshot();
+    await sim.navigateAndCompareSnapshots('.', images + "_bounds", [5]);
+    snapshot = await sim.clickBoth();
 
     data = Buffer.from('00000A00C60D5000' + 'C60D5000' + '00000A00C60D5000' + '0000EA600000EA60', 'hex');
     const tx = transport.send(0xe0, 0x41, 0x02, 0x00, data);
