c20eb0b9cdc2200d02d3e2d9dd4ea4a7d624afb1
Hjort <shj@concordium.com>
2022-04-06 15:55:22 +0200

    Fix #52 - Incorrect order in cooldownParameters

--- src/signUpdateCooldownParameters.c
+++ src/signUpdateCooldownParameters.c
@@ -9,11 +9,11 @@ static tx_state_t *tx_state = &global_tx_state;
 UX_STEP_NOCB(
     ux_sign_cooldown_parameters_1_step,
     bnnn_paging,
-    {.title = "Delegator cooldown", .text = (char *) global.signCooldownParameters.delegatorCooldown});
+    {.title = "Pool owner cooldown", .text = (char *) global.signCooldownParameters.poolOwnerCooldown});
 UX_STEP_NOCB(
     ux_sign_cooldown_parameters_2_step,
     bnnn_paging,
-    {.title = "Pool owner cooldown", .text = (char *) global.signCooldownParameters.poolOwnerCooldown});
+    {.title = "Delegator cooldown", .text = (char *) global.signCooldownParameters.delegatorCooldown});
 UX_FLOW(
     ux_sign_cooldown_parameters,
     &ux_sign_flow_shared_review,
@@ -29,16 +29,16 @@ void handleSignUpdateCooldownParameters(uint8_t *cdata, volatile unsigned int *f
     cx_sha256_init(&tx_state->hash);
     cdata += hashUpdateHeaderAndType(cdata, UPDATE_TYPE_COOLDOWN_PARAMETERS);
 
-    // RewardPeriodLength is a 64-bit number
-    uint64_t delegatorCooldown = U8BE(cdata, 0);
-    cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 8, NULL, 0);
-    cdata += 8;
-    numberToText(ctx->delegatorCooldown, sizeof(ctx->delegatorCooldown), delegatorCooldown);
-
-    // RewardPeriodLength is a 64-bit number
+    // Pool owner cooldown is a 64-bit number (represents seconds)
     uint64_t poolOwnerCooldown = U8BE(cdata, 0);
     cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 8, NULL, 0);
     numberToText(ctx->poolOwnerCooldown, sizeof(ctx->poolOwnerCooldown), poolOwnerCooldown);
+    cdata += 8;
+
+    // Delegator cooldown is a 64-bit number (represents seconds)
+    uint64_t delegatorCooldown = U8BE(cdata, 0);
+    cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 8, NULL, 0);
+    numberToText(ctx->delegatorCooldown, sizeof(ctx->delegatorCooldown), delegatorCooldown);
 
     ux_flow_init(0, ux_sign_cooldown_parameters, NULL);
     *flags |= IO_ASYNCH_REPLY;
