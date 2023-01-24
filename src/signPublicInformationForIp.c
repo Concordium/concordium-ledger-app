7e6c95df548135a678124945036356ed363d92f2
Hjort <shj@concordium.com>
2023-01-24 17:09:29 +0100

    Make signIpInfo initial screen be part of 1st key batch

--- src/signPublicInformationForIp.c
+++ src/signPublicInformationForIp.c
@@ -7,15 +7,18 @@
 static signPublicInformationForIp_t *ctx = &global.signPublicInformationForIp;
 static tx_state_t *tx_state = &global_tx_state;
 
-UX_STEP_CB(ux_sign_public_info_review, nn, sendSuccessNoIdle(), {"Review identity", "provider info"});
-UX_FLOW(ux_review_public_info_for_ip, &ux_sign_public_info_review);
-
 UX_STEP_CB(
     ux_sign_public_info_for_i_public_key_0_step,
     bnnn_paging,
     sendSuccessNoIdle(),
     {.title = "Public key", .text = (char *) global.signPublicInformationForIp.publicKey});
 UX_FLOW(ux_sign_public_info_for_i_public_key, &ux_sign_public_info_for_i_public_key_0_step);
+
+UX_STEP_CB(ux_sign_public_info_review, nn, sendSuccessNoIdle(), {"Review identity", "provider info"});
+UX_FLOW(
+    ux_review_public_info_for_ip,
+    &ux_sign_public_info_review,
+    &ux_sign_public_info_for_i_public_key_0_step);
 
 UX_STEP_CB(
     ux_sign_public_info_for_ip_sign,
@@ -66,9 +69,9 @@ void handleSignPublicInformationForIp(uint8_t *cdata, uint8_t p1, volatile unsig
         ctx->publicKeysLength = cdata[0];
         cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 1, NULL, 0);
 
+        ctx->showIntro = true;
         ctx->state = TX_PUBLIC_INFO_FOR_IP_VERIFICATION_KEY;
-        ux_flow_init(0, ux_review_public_info_for_ip, NULL);
-        *flags |= IO_ASYNCH_REPLY;
+        sendSuccessNoIdle();
     } else if (p1 == P1_VERIFICATION_KEY && ctx->state == TX_PUBLIC_INFO_FOR_IP_VERIFICATION_KEY) {
         if (ctx->publicKeysLength <= 0) {
             THROW(ERROR_INVALID_STATE);
@@ -91,7 +94,12 @@ void handleSignPublicInformationForIp(uint8_t *cdata, uint8_t p1, volatile unsig
         if (ctx->publicKeysLength == 0) {
             ctx->state = TX_PUBLIC_INFO_FOR_IP_THRESHOLD;
         }
-        ux_flow_init(0, ux_sign_public_info_for_i_public_key, NULL);
+        if (ctx->showIntro == false) {
+            ux_flow_init(0, ux_sign_public_info_for_i_public_key, NULL);
+        } else {
+            ctx->showIntro = false;
+            ux_flow_init(0, ux_review_public_info_for_ip, NULL);
+        }
         *flags |= IO_ASYNCH_REPLY;
     } else if (p1 == P1_THRESHOLD && ctx->state == TX_PUBLIC_INFO_FOR_IP_THRESHOLD) {
         // Read the threshold byte and parse it to display it.
