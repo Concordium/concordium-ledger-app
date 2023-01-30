e23c0c497737a827009d9c47c464ec6ffca6109a
Hjort <shj@concordium.com>
2023-01-30 13:00:39 +0100

    If only a single key is used for sign_public_for_ip, show everything in a single flow

--- src/signPublicInformationForIp.c
+++ src/signPublicInformationForIp.c
@@ -8,14 +8,14 @@ static signPublicInformationForIp_t *ctx = &global.signPublicInformationForIp;
 static tx_state_t *tx_state = &global_tx_state;
 
 UX_STEP_CB(
-    ux_sign_public_info_for_i_public_key_0_step,
+    ux_sign_public_info_for_ip_public_key_0_step,
     bnnn_paging,
     sendSuccessNoIdle(),
     {.title = "Public key", .text = (char *) global.signPublicInformationForIp.publicKey});
-UX_FLOW(ux_sign_public_info_for_i_public_key, &ux_sign_public_info_for_i_public_key_0_step);
+UX_FLOW(ux_sign_public_info_for_ip_public_key, &ux_sign_public_info_for_ip_public_key_0_step);
 
 UX_STEP_CB(ux_sign_public_info_review, nn, sendSuccessNoIdle(), {"Review identity", "provider info"});
-UX_FLOW(ux_review_public_info_for_ip, &ux_sign_public_info_review, &ux_sign_public_info_for_i_public_key_0_step);
+UX_FLOW(ux_review_public_info_for_ip, &ux_sign_public_info_review, &ux_sign_public_info_for_ip_public_key_0_step);
 
 UX_STEP_CB(
     ux_sign_public_info_for_ip_sign,
@@ -34,7 +34,15 @@ UX_STEP_NOCB(
     bn,
     {"Signature threshold", (char *) global.signPublicInformationForIp.threshold});
 UX_FLOW(
-    ux_sign_public_info_for_ip_threshold,
+    ux_sign_public_info_for_ip_final,
+    &ux_sign_public_info_for_ip_public_key_0_step,
+    &ux_sign_public_info_for_ip_threshold_0_step,
+    &ux_sign_public_info_for_ip_sign,
+    &ux_sign_public_info_for_ip_decline);
+UX_FLOW(
+    ux_sign_public_info_for_ip_complete,
+    &ux_sign_public_info_review,
+    &ux_sign_public_info_for_ip_public_key_0_step,
     &ux_sign_public_info_for_ip_threshold_0_step,
     &ux_sign_public_info_for_ip_sign,
     &ux_sign_public_info_for_ip_decline);
@@ -90,20 +98,26 @@ void handleSignPublicInformationForIp(uint8_t *cdata, uint8_t p1, volatile unsig
         ctx->publicKeysLength -= 1;
         if (ctx->publicKeysLength == 0) {
             ctx->state = TX_PUBLIC_INFO_FOR_IP_THRESHOLD;
-        }
-        if (ctx->showIntro == false) {
-            ux_flow_init(0, ux_sign_public_info_for_i_public_key, NULL);
+            sendSuccessNoIdle();
         } else {
-            ctx->showIntro = false;
-            ux_flow_init(0, ux_review_public_info_for_ip, NULL);
+            if (ctx->showIntro == false) {
+                ux_flow_init(0, ux_sign_public_info_for_ip_public_key, NULL);
+            } else {
+                ctx->showIntro = false;
+                ux_flow_init(0, ux_review_public_info_for_ip, NULL);
+            }
+            *flags |= IO_ASYNCH_REPLY;
         }
-        *flags |= IO_ASYNCH_REPLY;
     } else if (p1 == P1_THRESHOLD && ctx->state == TX_PUBLIC_INFO_FOR_IP_THRESHOLD) {
         // Read the threshold byte and parse it to display it.
         cx_hash((cx_hash_t *) &tx_state->hash, 0, cdata, 1, NULL, 0);
         bin2dec(ctx->threshold, sizeof(ctx->threshold), cdata[0]);
 
-        ux_flow_init(0, ux_sign_public_info_for_ip_threshold, NULL);
+        if (ctx->showIntro == false) {
+            ux_flow_init(0, ux_sign_public_info_for_ip_final, NULL);
+        } else {
+            ux_flow_init(0, ux_sign_public_info_for_ip_complete, NULL);
+       }
         *flags |= IO_ASYNCH_REPLY;
     } else {
         THROW(ERROR_INVALID_STATE);
