8983f04177604c8f900894115bc77598ad995388
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2024-12-03 16:54:24 +0100

    fix: lint

--- src/common/util.c
+++ src/common/util.c
@@ -295,10 +295,10 @@ void blsKeygen(const uint8_t *seed, size_t seedLength, uint8_t *dst, size_t dstL
     uint8_t sk[l_CONST];
     uint8_t prk[32];
     uint8_t salt[32] = {
-        66, 76, 83, 45, 83, 73, 71, 45, 75, 69, 89,
-        71, 69, 78, 45, 83, 65, 76, 84, 45};  // Initially set to the byte representation of
-                                              // "BLS-SIG-KEYGEN-SALT-"
-    size_t saltSize = 20;                     // 20 = size of initial salt seed
+        66, 76, 83, 45, 83, 73, 71, 45, 75, 69,
+        89, 71, 69, 78, 45, 83, 65, 76, 84, 45};  // Initially set to the byte representation of
+                                                  // "BLS-SIG-KEYGEN-SALT-"
+    size_t saltSize = 20;                         // 20 = size of initial salt seed
     uint8_t ikm[SEED_LENGTH + 1];
 
     memcpy(ikm, seed, SEED_LENGTH);
