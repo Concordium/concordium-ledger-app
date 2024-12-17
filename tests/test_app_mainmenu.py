aac63c15a7e108cba4c06dc17c4128e8546e6542
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2024-12-17 16:14:24 +0100

    fix: home screen - menu

--- tests/test_app_mainmenu.py
+++ tests/test_app_mainmenu.py
@@ -13,26 +13,9 @@ def test_app_mainmenu(firmware, navigator, test_name, default_screenshot_path):
             NavInsID.RIGHT_CLICK,
             NavInsID.RIGHT_CLICK,
         ]
-    elif firmware is Firmware.STAX:
-        instructions = [
-            NavInsID.USE_CASE_HOME_SETTINGS,
-            NavIns(NavInsID.TOUCH, (200, 113)),
-            NavIns(NavInsID.TOUCH, (200, 261)),
-            NavInsID.USE_CASE_CHOICE_CONFIRM,
-            NavIns(NavInsID.TOUCH, (200, 261)),
-            NavInsID.USE_CASE_SETTINGS_NEXT,
-            NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT,
-        ]
-    elif firmware is Firmware.FLEX:
-        instructions = [
-            NavInsID.USE_CASE_HOME_SETTINGS,
-            NavIns(NavInsID.TOUCH, (200, 113)),
-            NavIns(NavInsID.TOUCH, (200, 300)),
-            NavInsID.USE_CASE_CHOICE_CONFIRM,
-            NavIns(NavInsID.TOUCH, (200, 300)),
-            NavInsID.USE_CASE_SETTINGS_NEXT,
-            NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT,
-        ]
+    else:
+        instructions = [NavInsID.USE_CASE_HOME_SETTINGS, NavInsID.LEFT_HEADER_TAP]
+
     navigator.navigate_and_compare(
         default_screenshot_path,
         test_name,
