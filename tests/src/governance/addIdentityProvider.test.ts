4bfcbb1ea5cfc08bc6e1b01766610eedd7d8b65f
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-08-08 15:59:21 +0200

    update tests to compare all screens

--- tests/src/governance/addIdentityProvider.test.ts
+++ tests/src/governance/addIdentityProvider.test.ts
@@ -3,12 +3,13 @@ import Zemu from '@zondax/zemu';
 import { setupZemu } from './options';
 
 async function addIdentityProviderShared(sim: Zemu, transport: Transport) {
+    const name = `${sim.startOptions.model}_add_identity_provider`;
     // Initial pack
     let data = Buffer.from('080000045100000000000000000000000000000000000000020000000000000000000000000000000a00000000000000640000000063de5da7000000290d0000006200000001', 'hex');
     transport.send(0xe0, 0x2d, 0x00, 0x00, data);
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-    await sim.clickRight();
-    const snapshot1 = await sim.clickBoth(undefined, false);
+    await sim.navigateAndCompareSnapshots('.', `${name}/init`, [2]);
+    let snap = await sim.clickBoth(undefined, false);
 
     // Length of the name
     data = Buffer.from('00000009', 'hex');
@@ -17,8 +18,8 @@ async function addIdentityProviderShared(sim: Zemu, transport: Transport) {
     // The name
     data = Buffer.from('54657374206e616d65', 'hex');
     transport.send(0xe0, 0x2d, 0x02, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot1);
-    const snapshot2 = await sim.clickBoth(undefined, false);
+    await sim.waitUntilScreenIsNot(snap);
+    snap = await sim.clickBoth(undefined, false);
 
     // Length of the URL
     data = Buffer.from('00000015', 'hex');
@@ -28,10 +29,10 @@ async function addIdentityProviderShared(sim: Zemu, transport: Transport) {
     data = Buffer.from('687474703a2f2f636f6e636f726469756d2e636f6d', 'hex');
     transport.send(0xe0, 0x2d, 0x02, 0x00, data);
     Zemu.sleep(1000);
-    await sim.waitUntilScreenIsNot(snapshot2);
+    await sim.waitUntilScreenIsNot(snap);
     // This right click is unnecessary for Nano X, but does nothing
     await sim.clickRight(undefined, false);
-    const snapshot3 = await sim.clickBoth(undefined, false);
+    snap = await sim.clickBoth(undefined, false);
 
     // Length of the description
     data = Buffer.from('00000010', 'hex');
@@ -40,30 +41,26 @@ async function addIdentityProviderShared(sim: Zemu, transport: Transport) {
     // The description
     data = Buffer.from('54657374206465736372697074696f6e', 'hex');
     transport.send(0xe0, 0x2d, 0x02, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot3);
+    await sim.waitUntilScreenIsNot(snap);
     return sim.clickBoth(undefined, false);
 }
 
 test('[NANO S] Add identity provider', setupZemu('nanos', async (sim, transport) => {
-    const snapshot4 = await addIdentityProviderShared(sim, transport);
+    const name = `${sim.startOptions.model}_add_identity_provider`;
+    let snap = await addIdentityProviderShared(sim, transport);
 
     // Verify key
     let data = Buffer.from('00000010', 'hex');
     transport.send(0xe0, 0x2d, 0x03, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot4);
-    await sim.clickRight();
-    await sim.clickRight();
-    await sim.clickRight();
-    const snapshot5 = await sim.clickBoth(undefined, false);
+    await sim.waitUntilScreenIsNot(snap);
+    await sim.navigateAndCompareSnapshots('.', `${name}/verify_key`, [3]);
+    snap = await sim.clickBoth(undefined, false);
 
     // CDI key
     data = Buffer.from('37efcc5b9180fc9c43a5a51a2f27d6581e63e4b2b3dad75b8510061b8c2db39f', 'hex');
     const tx = transport.send(0xe0, 0x2d, 0x04, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot5);
-    await sim.clickRight();
-    await sim.clickRight();
-    await sim.clickRight();
-    await sim.clickRight();
+    await sim.waitUntilScreenIsNot(snap);
+    await sim.navigateAndCompareSnapshots('.', `${name}/cdi_key`, [4]);
     await sim.clickBoth(undefined, false);
 
     await expect(tx).resolves.toEqual(
@@ -72,21 +69,21 @@ test('[NANO S] Add identity provider', setupZemu('nanos', async (sim, transport)
 }));
 
 test('[NANO SP] Add identity provider', setupZemu('nanosp', async (sim, transport) => {
-    const snapshot4 = await addIdentityProviderShared(sim, transport);
+    const name = `${sim.startOptions.model}_add_identity_provider`;
+    let snap = await addIdentityProviderShared(sim, transport);
 
     // Verify key
     let data = Buffer.from('00000010', 'hex');
     transport.send(0xe0, 0x2d, 0x03, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot4);
-    await sim.clickRight();
-    const snapshot5 = await sim.clickBoth(undefined, false);
+    await sim.waitUntilScreenIsNot(snap);
+    await sim.navigateAndCompareSnapshots('.', `${name}/verify_key`, [1]);
+    snap = await sim.clickBoth(undefined, false);
 
     // CDI key
     data = Buffer.from('37efcc5b9180fc9c43a5a51a2f27d6581e63e4b2b3dad75b8510061b8c2db39f', 'hex');
     const tx = transport.send(0xe0, 0x2d, 0x04, 0x00, data);
-    await sim.waitUntilScreenIsNot(snapshot5);
-    await sim.clickRight();
-    await sim.clickRight();
+    await sim.waitUntilScreenIsNot(snap);
+    await sim.navigateAndCompareSnapshots('.', `${name}/cdi_key`, [2]);
     await sim.clickBoth(undefined, false);
 
     await expect(tx).resolves.toEqual(
--- tests/src/governance/updateEuroPerEnergy.test.ts
+++ tests/src/governance/updateEuroPerEnergy.test.ts
@@ -3,12 +3,11 @@ import Zemu from '@zondax/zemu';
 import { setupZemu } from './options';
 
 async function updateEuroPerEnergy(sim: Zemu, transport: Transport) {
+    const name = `${sim.startOptions.model}_euro_per_energy`;
     const data = Buffer.from('080000045100000000000000000000000000000000000000020000000000000000000000000000000a00000000000000640000000063de5da70000002903000000000000000100000000000000f2', 'hex');
     const tx = transport.send(0xe0, 0x06, 0x00, 0x00, data);
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-    await sim.clickRight();
-    await sim.clickRight();
-    await sim.clickBoth();
+    await sim.navigateAndCompareSnapshots('.', name, [2, 0]);
     await expect(tx).resolves.toEqual(
         Buffer.from('fb3b5c56421c488ccf8d77b7d384856cbfb6b15095628cf1758d08c33b8ec63d222b0c2ea5394a84512b872ffb7cd5922e8addd1aa8a218283ad73e34cf463049000', 'hex'),
     );
