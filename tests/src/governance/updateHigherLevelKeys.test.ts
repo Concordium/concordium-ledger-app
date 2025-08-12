cf11aae24fc03fc797c9b4425cb8a1926341e327
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-08-12 15:16:50 +0200

    Add snapshots for higher level keys update type

--- tests/src/governance/updateHigherLevelKeys.test.ts
+++ tests/src/governance/updateHigherLevelKeys.test.ts
@@ -5,26 +5,29 @@ import { setupZemu } from './options';
 async function updateHigherLevelKeys(
     sim: Zemu, transport: Transport,
     ins: number,
+    level: string,
     updateType: string,
     keyUpdateType: string,
     expectedSignature: string,
-    handleKeyUi: () => Promise<any>,
+    handleKeyUi: (i: number) => Promise<any>,
 ) {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
     let data = Buffer.from('080000045100000000000000000000000000000000000000020000000000000000000000000000000a00000000000000640000000063de5da700000029', 'hex');
     data = Buffer.concat([data, Buffer.from(updateType, 'hex'), Buffer.from(keyUpdateType, 'hex'), Buffer.from('0002', 'hex')]);
     transport.send(0xe0, ins, 0x00, 0x00, data);
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-    await sim.clickRight();
+    await sim.navigateAndCompareSnapshots('.', `${name}/${level}/init`, [1])
     let snapshot = await sim.clickBoth(undefined, false);
 
     const key1 = Buffer.from('00b6bc751f1abfb6440ff5cce27d7cdd1e7b0b8ec174f54de426890635b27e7daf', 'hex');
     const key2 = Buffer.from('0046a3e38ddf8b493be6e979034510b91db5448da9cba48c106139c288d658a004', 'hex');
     const keys = [key1, key2];
 
-    for (const key of keys) {
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
         transport.send(0xe0, ins, 0x01, 0x00, key);
         await sim.waitUntilScreenIsNot(snapshot);
-        snapshot = await handleKeyUi();
+        snapshot = await handleKeyUi(i);
     }
 
     data = Buffer.from('0002', 'hex');
@@ -39,17 +42,18 @@ async function updateHigherLevelKeys(
 }
 
 test('[NANO S] Update root keys', setupZemu('nanos', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = 'root_root';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x28,
+        level,
         '0A',
         '00',
         'dec8a3bfc46ee9c374c78d83ab1e4ce5af0461017c476b011863ee969cbd52b833cc0a32d2411bcade7940c754c3c25d33dad7260517409f7700982eb5b81e029000',
-        async () => {
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [3])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
@@ -57,15 +61,18 @@ test('[NANO S] Update root keys', setupZemu('nanos', async (sim, transport) => {
 }));
 
 test('[NANO SP] Update root keys', setupZemu('nanosp', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = 'root_root';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x28,
+        level,
         '0A',
         '00',
         'dec8a3bfc46ee9c374c78d83ab1e4ce5af0461017c476b011863ee969cbd52b833cc0a32d2411bcade7940c754c3c25d33dad7260517409f7700982eb5b81e029000',
-        async () => {
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [1])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
@@ -73,17 +80,18 @@ test('[NANO SP] Update root keys', setupZemu('nanosp', async (sim, transport) =>
 }));
 
 test('[NANO S] Update level 1 with root keys', setupZemu('nanos', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = '1_root';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x28,
+        level,
         '0A',
         '01',
         '54e9acf8880101da599f908aa55bff5c5e5bc1a1ce186cd1136d227e7370a42529bd1c1238aff0d603dce787fb705e043e7ef560ae12fabc0a9f22e21966d8099000',
-        async () => {
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [3])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
@@ -91,15 +99,18 @@ test('[NANO S] Update level 1 with root keys', setupZemu('nanos', async (sim, tr
 }));
 
 test('[NANO SP] Update level 1 keys with root keys', setupZemu('nanosp', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = '1_root';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x28,
+        level,
         '0A',
         '01',
         '54e9acf8880101da599f908aa55bff5c5e5bc1a1ce186cd1136d227e7370a42529bd1c1238aff0d603dce787fb705e043e7ef560ae12fabc0a9f22e21966d8099000',
-        async () => {
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [1])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
@@ -107,17 +118,18 @@ test('[NANO SP] Update level 1 keys with root keys', setupZemu('nanosp', async (
 }));
 
 test('[NANO S] Update level 1 with level 1 keys', setupZemu('nanos', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = '1_1';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x29,
+        level,
         '0B',
         '00',
         '8652d8ce5c3385712a636e0af7297b9355f3f0e7191220a8b1db337f31ac04bb1fc7f2dbc1e326e03f37e3e586c54c53c43428e874e713cd04a06b82094f49099000',
-        async () => {
-            await sim.clickRight();
-            await sim.clickRight();
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [3])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
@@ -125,15 +137,18 @@ test('[NANO S] Update level 1 with level 1 keys', setupZemu('nanos', async (sim,
 }));
 
 test('[NANO SP] Update level 1 keys with level 1 keys', setupZemu('nanosp', async (sim, transport) => {
+    const name = `${sim.startOptions.model}_higher_level_keys`;
+    const level = '1_1';
     await updateHigherLevelKeys(
         sim,
         transport,
         0x29,
+        level,
         '0B',
         '00 ',
         '8652d8ce5c3385712a636e0af7297b9355f3f0e7191220a8b1db337f31ac04bb1fc7f2dbc1e326e03f37e3e586c54c53c43428e874e713cd04a06b82094f49099000',
-        async () => {
-            await sim.clickRight();
+        async (i) => {
+            await sim.navigateAndCompareSnapshots('.', `${name}/${level}/${i}`, [1])
             await sim.clickRight(undefined, false);
             return sim.clickBoth(undefined, false);
         },
